a7b17da5e81f725d56f8e54ae451978afb3c0d69
Max K <mxkk@pm.me>
2026-03-09 15:45:22 -0400

    Implement encode and decode, without wrapping A-Za-z

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,10 +1,17 @@
 # Caesar Cipher 2026 @mistermaxk
-# key => 0-25 (x+n) % 26
-# wrap A-Z a-z
-# skip puncuatation and spaces
-# negative key to decode (x-n) % 26
+# wrap A-Z a-z 65 - 122
+
 def caesar_cipher(str, key = 0)
-  
+  encoded = str.split('').map do |ch|
+    if /[A-Za-z]/.match?(ch)
+      (ch.getbyte(0) + key)
+      # wrap to 65-122
+    else
+      ch.getbyte(0)
+    end
+  end
+  encoded_string = encoded.map { |ch| ch.chr("utf-8") }.join("")
+  encoded_string
 end
 
 #test cases
