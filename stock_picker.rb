3e8a2582d1c9f2b7bd49a900b27ae153178e782b
Max K <mxkk@pm.me>
2026-03-12 21:33:12 -0400

    Create boilerplate for stock_picker.rb

---
+++ stock_picker.rb
@@ -0,0 +1,9 @@
+# Stock Picker 2026 @mistermaxk
+# Return a pair of days - best day to buy (low), best day to sell(high)
+# You need to buy before you can sell
+def stock_picker(prices)
+  # find lowest, the find highest sequential
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
+# => [1,4]  # for a profit of $15 - $3 == $12
